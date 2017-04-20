4f6fbac2a23587698b0775d64872c8d8201290b4
July <jurcpdo@gmail.com>
2017-04-20 19:49:02 +0200

    Testing git

--- app/src/main/java/com/example/trogs/piggysplit/MyBills.java
+++ app/src/main/java/com/example/trogs/piggysplit/MyBills.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MyBills extends AppCompatActivity {
-
+	// Testing git
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
